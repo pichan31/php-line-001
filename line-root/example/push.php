acc4a109c837e1019131ec9769f06c877ed08be0
pichan31 <37392081+pichan31@users.noreply.github.com>
2018-03-30 15:43:27 +0700

    Update push.php

--- line-root/example/push.php
+++ line-root/example/push.php
@@ -7,12 +7,6 @@ print_r($_GET);
 include ('../config.php');
 include ('../line-bot.php');
 
-// // AAA info
-// $channelSecret = '284c868918e2a89b8e9d311c8f7ccdf3';
-// $access_token  = 'bkL15evcI4ilmJveNzTW1KAUi5Wuir6z62Rj+cW6YKQmrJUVUGXbhwHdYQJRY7GHuSO8oTKr2v6P1mR3rFm0Wqs9DsO0tmamkbQgCw4gnXmQwmZ/CRCxGV4wgoKzZszjQI/pn2Yr+E9NB3ytWEYMIAdB04t89/1O/w1cDnyilFU=';
-
-$channelSecret = 'a0723beec81191cfa84532647eda4490';
-$access_token  = '+FPbwVPcxPAUeI9hWgihaa+SbMxPrqFCF8l9ljKQuVLKwj7NwiG5lPrj3Hzbk6SD33zAdxi+nnbhtznPtX+8M+MeDrvJ3+5yS1uF6xATQ3mvCZs/+7K6ysy+9Mxqjp+YtkGdqSryBMNdJd2tkmNX+gdB04t89/1O/w1cDnyilFU=';
 
 $bot = new BOT_API($channelSecret, $access_token);
 
