38afa6149c40271e23f804fe31b666e62f1dab98
pichan31 <37392081+pichan31@users.noreply.github.com>
2018-03-30 16:42:51 +0700

    Add files via upload

---
+++ php-root/example/config.php
@@ -0,0 +1,5 @@
+<?php
+
+$channelSecret = 'a0723beec81191cfa84532647eda4490';
+$access_token  = '+FPbwVPcxPAUeI9hWgihaa+SbMxPrqFCF8l9ljKQuVLKwj7NwiG5lPrj3Hzbk6SD33zAdxi+nnbhtznPtX+8M+MeDrvJ3+5yS1uF6xATQ3mvCZs/+7K6ysy+9Mxqjp+YtkGdqSryBMNdJd2tkmNX+gdB04t89/1O/w1cDnyilFU=';
+?>
---
+++ php-root/example/push-test.php
@@ -0,0 +1,26 @@
+<?php
+echo "push.php?id=xxxxxxxxxxxxx&msg=xxxxxxxx"."<hr>";
+print_r($_GET);
+
+include ('config.php');
+include ('../line-bot.php');
+
+$bot = new BOT_API($channelSecret, $access_token);
+
+$line_id = $_GET['id'];
+$msg = $_GET['msg'];
+
+if(!isset($_GET['id'])) { $bot->sendMessageNew('U08b751e137f4889902a101831ad96fda', 'Hello World !! - date U '.date('U'));
+} else $bot->sendMessageNew($line_id, $msg);
+
+if ($bot->isSuccess()) {
+    echo 'Succeeded!';
+    exit();
+}
+
+// Failed
+echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
+exit();
+
+
+?>
