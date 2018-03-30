18ddfa488f2c2d249cdff1609cccd9825118ed92
pichan31 <37392081+pichan31@users.noreply.github.com>
2018-03-30 18:03:29 +0700

    Update config.php

--- php-root/example-green/config.php
+++ php-root/example-green/config.php
@@ -6,5 +6,5 @@
 
 // green 
 $channelSecret = '284c868918e2a89b8e9d311c8f7ccdf3';
-$access_token  = 'bkL15evcI4ilmJveNzTW1KAUi5Wuir6z62Rj+cW6YKQmrJUVUGXbhwHdYQJRY7GHuSO8oTKr2v6P1mR3rFm0Wqs9DsO0tmamkbQgCw4gnXmQwmZ/CRCxGV4wgoKzZszjQI/pn2Yr+E9NB3ytWEYMIAdB04t89/1O/w1cDnyilFU=";
+$access_token  = 'bkL15evcI4ilmJveNzTW1KAUi5Wuir6z62Rj+cW6YKQmrJUVUGXbhwHdYQJRY7GHuSO8oTKr2v6P1mR3rFm0Wqs9DsO0tmamkbQgCw4gnXmQwmZ/CRCxGV4wgoKzZszjQI/pn2Yr+E9NB3ytWEYMIAdB04t89/1O/w1cDnyilFU=';
 ?>
