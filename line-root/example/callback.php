8a4c13ea0d88511b72b3384c33d0308aef08ffea
pichan31 <37392081+pichan31@users.noreply.github.com>
2018-03-30 15:41:53 +0700

    Create callback.php

---
+++ line-root/example/callback.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+
+
+include ('../config.php');
+include ('../line-bot.php');
+
+$bot = new BOT_API($channelSecret, $access_token);
+	
+if (!empty($bot->isEvents)) {
+		
+    $bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
+
+    if ($bot->isSuccess()) {
+        echo 'Succeeded!';
+        exit();
+    }
+
+    // Failed
+    echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
+    exit();
+
+}
+
+
+?>
